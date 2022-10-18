363bd1e930688f7b1275b05c7df2c4cdbb802b78
Purveh Choudhary <98455640+Purvesh-choudhary@users.noreply.github.com>
2022-10-18 09:18:01 +0530

    Create Table11t20.cpp

---
+++ tips/Table11t20.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+#include<conio.h>
+int main()
+{
+    int t;
+    for(int i=1; i<=10; i++)
+    {
+        for(int j=11; j<=20; j++)
+        {
+            cout<<j*i<<"\t";
+        }
+        cout<<endl;
+    }
+    return 0;
+}  
