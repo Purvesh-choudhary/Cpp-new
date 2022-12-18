006f97587796e06df8da14a403a2225d1641df0b
Purveh Choudhary <98455640+Purvesh-choudhary@users.noreply.github.com>
2022-12-18 19:22:46 +0530

    Create autoOmitData.cpp

---
+++ DAY/autoOmitData.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    auto a=1;
+    auto b=1.1;
+    auto c=true;
+    auto d='P';
+
+    cout<<"the value of a = "<<a<<endl;
+    cout<<"the value of b = "<<b<<endl;
+    cout<<"the value of c = "<<c<<endl;
+    cout<<"the value of d = "<<d<<endl;
+}
